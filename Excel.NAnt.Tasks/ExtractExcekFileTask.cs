1cc17ee0b04d7548ce3d1d8a7b152f04671c8766
Ben O'Hara <bpo@robotparade.co.uk>
2011-07-29 16:00:29 +0100

    Started adding tasks for exploding an Excel file.

---
+++ Excel.NAnt.Tasks/ExtractExcekFileTask.cs
@@ -0,0 +1,20 @@
+﻿namespace Excel.NAnt.Tasks
+{
+    using System;
+    using global::NAnt.Core;
+    using global::NAnt.Core.Attributes;
+    using global::NAnt.Core.Types;
+    using XL = Microsoft.Office.Interop.Excel;
+    using System.IO;
+    using System.Collections.Generic;
+
+
+    [TaskName("ExtractExcelFile")]
+    class ExtractExcekFileTask:Task
+    {
+        protected override void ExecuteTask()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
